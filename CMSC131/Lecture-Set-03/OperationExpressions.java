2a860f79a7a96761bf453631a2d2567234cb1705
etaylor5 <etaylorsubs1993@gmail.com>
2024-05-01 14:25:49 -0400

    + added Lecture Set 3: Operations and Methods

---
+++ CMSC131/Lecture-Set-03/OperationExpressions.java
@@ -0,0 +1,18 @@
+public class OperationExpressions {
+
+	/**
+	 * Will show how assignment statements are
+	 * also expressions.
+	 */
+	public static void main(String[] args) {
+		int x = 5;
+		char y = 'a';
+		boolean z = true;
+		
+		System.out.print("x: ");
+		System.out.println(x = 7);
+		System.out.println("y: " + (y = 'x'));
+		System.out.println("z: " + (z = false));
+		System.out.println("z again: " + (z == false));
+	}
+}
---
+++ CMSC131/Lecture-Set-03/PrePostCompare.java
@@ -0,0 +1,34 @@
+public class PrePostCompare {
+
+	/**
+     * Will compare the pre and post
+     * increment operators
+	 */
+	public static void main(String[] args) {
+		int x, y;
+		
+		x = 3;
+		y = 5;
+		System.out.println("x = " + x + "; y = " + y);
+		System.out.println("x++ * y++ = " + (x++ * y++));
+		System.out.println("x = " + x + "; y = " + y + "\n");
+	
+		x = 3;
+		y = 5;
+		System.out.println("x = " + x + "; y = " + y);
+		System.out.println("++x * ++y = " + (++x * ++y));
+		System.out.println("x = " + x + "; y = " + y + "\n");
+	
+		x = 3;
+		y = 5;
+		System.out.println("x = " + x + "; y = " + y);
+		System.out.println("++x * y++ = " + (++x * y++));
+		System.out.println("x = " + x + "; y = " + y + "\n");
+	
+		x = 3;
+		y = 5;
+		System.out.println("x = " + x + "; y = " + y);
+		System.out.println("x++ * ++y = " + (x++ * ++y));
+		System.out.println("x = " + x + "; y = " + y + "\n");
+	}
+}
