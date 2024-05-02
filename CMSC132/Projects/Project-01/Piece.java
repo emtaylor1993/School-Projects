1ae5248048d0864d020c4a9c0ccaf6719bd91dbb
etaylor5 <etaylorsubs1993@gmail.com>
2024-05-01 22:28:16 -0400

    + added Project 1: Othello

---
+++ CMSC132/Projects/Project-01/Piece.java
@@ -0,0 +1,16 @@
+public enum Piece {
+    BLACK, WHITE, NONE;
+  
+    public String toString() {
+        switch (this) {
+            case BLACK:
+                return "black";
+            case WHITE:
+                return "white";
+            case NONE:
+                return "";
+        }
+  
+        return "";  // make compiler happy even though we should never reach here
+    }
+}
---
+++ CMSC132/Projects/Project-01/PlayGame.java
@@ -0,0 +1,6 @@
+public class PlayGame {
+    public static void main(String args[]) {
+        OthelloGUI othelloGUI= new OthelloGUI();
+        othelloGUI.play();
+    }
+}
