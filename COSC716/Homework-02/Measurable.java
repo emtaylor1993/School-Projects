94fafa4c9cc94c68de255e3b16880c4db92c4abf
etaylor5 <etaylorsubs1993@gmail.com>
2024-10-29 11:13:49 -0400

    + Changed name of directory

---
+++ COSC716/Homework-02/Measurable.java
@@ -0,0 +1,16 @@
+/**
+ * MEASURABLE
+ * 
+ * @author Emmanuel Taylor
+ * 
+ * @description
+ *      This abstract class represents a Measurable object that returns
+ *      the measurements of various objects.
+ * 
+ * @packages
+ *      N/A
+ */
+
+public interface Measurable {
+    String getMeasure();
+}
